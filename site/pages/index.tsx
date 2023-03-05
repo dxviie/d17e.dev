81084827439e95eebca0f27c29249cac3ca21bfd
david <david@d17e.dev>
2023-03-05 15:05:13 +0100

    added umami script tag

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next'
 import About from "../components/About";
+import Script from "next/script";
 
 const Home: NextPage = () => {
   return (
-      <main>
-          <About />
-      </main>
+      <>
+          <Script strategy="afterInteractive" data-website-id="fefcc6cd-205e-4632-940b-cdea83a38ac8" src="https://umami.d17e.dev/umami.js" />
+          <main>
+              <About />
+          </main>
+      </>
   )
 }
 
