40cc39847351ac9dbd7e7bd4ac46b43f0c13e3ec
david <david@d17e.dev>
2023-05-21 15:06:55 +0200

    specified time formatter locale (en) : fixed prod hydration errors?

--- site/services/DateTimeFormatter.ts
+++ site/services/DateTimeFormatter.ts
@@ -3,5 +3,5 @@ import moment from "moment";
 const DATE_FORMAT = "MMMM D YYYY";
 
 export const formatDate = (date: string): string => {
-  return moment(date).format(DATE_FORMAT);
+  return moment(date).locale("en").format(DATE_FORMAT);
 };
