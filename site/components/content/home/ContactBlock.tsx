a0af6f8bcb329e8b652d6a798551f1ceae9f8004
david <david@d17e.dev>
2023-05-21 17:57:05 +0200

    capturing exceptions ?

--- site/components/content/home/ContactBlock.tsx
+++ site/components/content/home/ContactBlock.tsx
@@ -1,6 +1,7 @@
 import { Button, VStack } from "@chakra-ui/react";
 import useThemeColors from "../../../styles/useThemeColors";
 import EmailSubscriptionForm from "../../core/interactive/EmailSubscriptionForm";
+import { captureException } from "@sentry/nextjs";
 
 export default function ContactBlock() {
   const colors = useThemeColors();
@@ -19,7 +20,12 @@ export default function ContactBlock() {
           <EmailSubscriptionForm />
           <Button
             onClick={() => {
-              throw new Error("well have ya ever");
+              try {
+                throw new Error("well have ya ever");
+              } catch (e) {
+                console.log(e);
+                captureException(e);
+              }
             }}
           >
             Boom!
