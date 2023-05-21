9704949d21e16c9b2a43296790dc823de33ea4b2
david <david@d17e.dev>
2023-05-21 15:58:22 +0200

    not rendering dates anymore

--- site/services/DateTimeFormatter.ts
+++ site/services/DateTimeFormatter.ts
@@ -4,5 +4,6 @@ const DATE_FORMAT = "MMMM D YYYY";
 
 export const formatDate = (date: string): string => {
   // specifying the locale makes sure we don't get hydration errors due to server/client locale mismatch
-  return moment(date).locale("en").format(DATE_FORMAT);
+  const formatted = moment(date).locale("en").format(DATE_FORMAT);
+  return "??";
 };
