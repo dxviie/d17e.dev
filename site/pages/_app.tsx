2b8514236f42906b82ace38b26e3f7e6ee0f3d87
david <david@d17e.dev>
2023-05-21 14:39:54 +0200

    removed hello api route + react strict mode wrapper

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -9,6 +9,7 @@ import { RecoilRoot } from "recoil";
 import d17eTheme from "../styles/d17eTheme";
 import Navigation from "../components/core/interactive/Navigation";
 import Script from "next/script";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -19,41 +20,43 @@ function MyApp({ Component, pageProps }: AppProps) {
         data-website-id="fefcc6cd-205e-4632-940b-cdea83a38ac8"
         src={"https://umami.d17e.dev/umami.js"}
       />
-      <RecoilRoot>
-        <ChakraProvider theme={d17eTheme} resetCSS={true}>
-          {/* headers */}
-          <Head>
-            <title>d17e.dev - code. art. ideas.</title>
-            <meta name="description" content="d17e.dev - code. art. ideas." />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-            <link rel="icon" href="/d17e-favicon.ico" />
-          </Head>
+      <React.StrictMode>
+        <RecoilRoot>
+          <ChakraProvider theme={d17eTheme} resetCSS={true}>
+            {/* headers */}
+            <Head>
+              <title>d17e.dev - code. art. ideas.</title>
+              <meta name="description" content="d17e.dev - code. art. ideas." />
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+              />
+              <link rel="icon" href="/d17e-favicon.ico" />
+            </Head>
 
-          <HStack zIndex={"1"} position={"absolute"} top={"5"} right={"5"}>
-            <Navigation></Navigation>
-          </HStack>
+            <HStack zIndex={"1"} position={"absolute"} top={"5"} right={"5"}>
+              <Navigation></Navigation>
+            </HStack>
 
-          {/* content */}
-          <Flex
-            direction={"column"}
-            align={"center"}
-            minH={"100vh"}
-            width={"100%"}
-            // scrollSnapType={"y mandatory"}
-          >
-            {/*<Box height={"5rem"}></Box> /!* navigation spacer *!/*/}
-            <Component {...pageProps} />
-            <Spacer />
-            <Footer />
-          </Flex>
+            {/* content */}
+            <Flex
+              direction={"column"}
+              align={"center"}
+              minH={"100vh"}
+              width={"100%"}
+              // scrollSnapType={"y mandatory"}
+            >
+              {/*<Box height={"5rem"}></Box> /!* navigation spacer *!/*/}
+              <Component {...pageProps} />
+              <Spacer />
+              <Footer />
+            </Flex>
 
-          {/* analytics, scripts,... */}
-          <Analytics />
-        </ChakraProvider>
-      </RecoilRoot>
+            {/* analytics, scripts,... */}
+            <Analytics />
+          </ChakraProvider>
+        </RecoilRoot>
+      </React.StrictMode>
     </>
   );
 }
