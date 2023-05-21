663cb2dd37f9b72a47e480bffe7d4f57d13b1d1d
david <david@d17e.dev>
2023-05-21 16:52:27 +0200

    added warning log

--- site/services/DateTimeFormatter.ts
+++ site/services/DateTimeFormatter.ts
@@ -5,5 +5,6 @@ const DATE_FORMAT = "MMMM D YYYY";
 export const formatDate = (date: string): string => {
   // specifying the locale makes sure we don't get hydration errors due to server/client locale mismatch
   const formatted = moment(date).locale("en").format(DATE_FORMAT);
+  console.warn("---> formatted date: ", formatted);
   return formatted;
 };
