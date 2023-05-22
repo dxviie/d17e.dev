19a6f2e11f7cd6e3b2f045269dd5655229cf49f1
david <david@d17e.dev>
2023-05-22 09:26:02 +0200

    momentjs locale customization: no dates

--- site/services/DateTimeFormatter.ts
+++ site/services/DateTimeFormatter.ts
@@ -1,42 +1,41 @@
-import moment from "moment";
-
 const DATE_FORMAT = "D YYYY";
 
 export const formatDate = (date: string): string => {
   // specifying the locale makes sure we don't get hydration errors due to server/client locale mismatch
-  return (
-    moment(date)
-      // @ts-ignore
-      .locale("en", {
-        months: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December",
-        ],
-        monthsShort: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December",
-        ],
-      })
-      .format(DATE_FORMAT)
-  );
+  // return (
+  //   moment(date)
+  //     // @ts-ignore
+  //     .locale("en", {
+  //       months: [
+  //         "January",
+  //         "February",
+  //         "March",
+  //         "April",
+  //         "May",
+  //         "June",
+  //         "July",
+  //         "August",
+  //         "September",
+  //         "October",
+  //         "November",
+  //         "December",
+  //       ],
+  //       monthsShort: [
+  //         "January",
+  //         "February",
+  //         "March",
+  //         "April",
+  //         "May",
+  //         "June",
+  //         "July",
+  //         "August",
+  //         "September",
+  //         "October",
+  //         "November",
+  //         "December",
+  //       ],
+  //     })
+  //     .format(DATE_FORMAT)
+  // );
+  return "?";
 };
