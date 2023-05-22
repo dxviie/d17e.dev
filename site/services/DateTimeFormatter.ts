994b95c4d8b66aaaec25af5c514177e6669ed273
david <david@d17e.dev>
2023-05-22 09:30:33 +0200

    custom date formatter

--- site/services/DateTimeFormatter.ts
+++ site/services/DateTimeFormatter.ts
@@ -1,41 +1,23 @@
+import moment from "moment";
+
 const DATE_FORMAT = "D YYYY";
 
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 export const formatDate = (date: string): string => {
-  // specifying the locale makes sure we don't get hydration errors due to server/client locale mismatch
-  // return (
-  //   moment(date)
-  //     // @ts-ignore
-  //     .locale("en", {
-  //       months: [
-  //         "January",
-  //         "February",
-  //         "March",
-  //         "April",
-  //         "May",
-  //         "June",
-  //         "July",
-  //         "August",
-  //         "September",
-  //         "October",
-  //         "November",
-  //         "December",
-  //       ],
-  //       monthsShort: [
-  //         "January",
-  //         "February",
-  //         "March",
-  //         "April",
-  //         "May",
-  //         "June",
-  //         "July",
-  //         "August",
-  //         "September",
-  //         "October",
-  //         "November",
-  //         "December",
-  //       ],
-  //     })
-  //     .format(DATE_FORMAT)
-  // );
-  return "?";
+  const myMoment = moment(date);
+  return `{MONTHS[myMoment.month()]} ${myMoment.day()} ${myMoment.year()}`;
 };
