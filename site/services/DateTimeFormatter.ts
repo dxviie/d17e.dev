66eaf779472c38502fde65dcba30dc39be30e695
david <david@d17e.dev>
2023-05-22 09:23:17 +0200

    momentjs locale customization: no months

--- site/services/DateTimeFormatter.ts
+++ site/services/DateTimeFormatter.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-const DATE_FORMAT = "MMMM D YYYY";
+const DATE_FORMAT = "D YYYY";
 
 export const formatDate = (date: string): string => {
   // specifying the locale makes sure we don't get hydration errors due to server/client locale mismatch
