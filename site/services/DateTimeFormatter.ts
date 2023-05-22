de71fdfa67f1aca2fb72dd5103f2d9bea062a6f4
david <david@d17e.dev>
2023-05-22 09:15:43 +0200

    momentjs locale customization: month names

--- site/services/DateTimeFormatter.ts
+++ site/services/DateTimeFormatter.ts
@@ -4,5 +4,25 @@ const DATE_FORMAT = "MMMM D YYYY";
 
 export const formatDate = (date: string): string => {
   // specifying the locale makes sure we don't get hydration errors due to server/client locale mismatch
-  return moment(date).locale("en").format(DATE_FORMAT);
+  return (
+    moment(date)
+      // @ts-ignore
+      .locale("en", {
+        months: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ],
+      })
+      .format(DATE_FORMAT)
+  );
 };
