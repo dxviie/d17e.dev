b2a0573ffeda965c43003bf6e6e423049bb71309
david <david@d17e.dev>
2023-06-22 18:40:12 +0200

    tumbleweed 404

--- site/pages/404.tsx
+++ site/pages/404.tsx
@@ -1,3 +1,20 @@
+import styles from "./404.module.css";
+
 export default function Custom404() {
-  return <div>404 - nothing to see here...</div>;
+  return (
+    <>
+      <div>404 - nothing to see here...</div>
+      <div className={styles.container}>
+        <div className={styles.traveller}>
+          <div className={styles.bouncingBall}>
+            <img
+              src={"429830.jpg"}
+              alt={"tumbleweed"}
+              className={styles.tumbleweed}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
