fcb559e6d484f3b685ca0b3cbb5a8362dd248ac6
david <david@d17e.dev>
2023-07-09 15:40:13 +0200

    wrapping up the 404

--- site/pages/404.tsx
+++ site/pages/404.tsx
@@ -1,25 +1,50 @@
 import styles from "./404.module.css";
-import { Spacer } from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import { Box, Center, Spacer, useColorMode, VStack } from "@chakra-ui/react";
+import { bodyFont, headerFont } from "../styles/fonts";
+import ArrowLink from "../components/core/interactive/ArrowLink";
 
 export default function Custom404() {
-  const router = useRouter();
+  const colorModeContext = useColorMode();
+  const imgResource =
+    colorModeContext.colorMode === "light"
+      ? "/tumbleweed-black.png"
+      : "/tumbleweed-white.png";
   return (
     <>
-      <div>404 - nothing to see here...</div>
-      <Spacer />
-      <div className={styles.container}>
-        <div className={styles.traveller}>
-          <div className={styles.bouncingBall}>
-            <img
-              // src={"429830.jpg"}
-              src={"/tumbleweed2.png"}
-              alt={"tumbleweed"}
-              className={styles.tumbleweed}
-            />
+      <Center>
+        <VStack width={"100vw"} height={"50vh"}>
+          <VStack>
+            <Box fontFamily={bodyFont.style.fontFamily} fontSize={"6rem"}>
+              404
+            </Box>
+            <Box
+              fontFamily={headerFont.style.fontFamily}
+              paddingBottom={"2rem"}
+            >
+              Nothing to see here...
+            </Box>
+          </VStack>
+          <Box height={"1rem"} />
+          <div className={styles.container}>
+            <div className={styles.traveller}>
+              <div className={styles.bouncingBall}>
+                <img
+                  src={imgResource}
+                  alt={"tumbleweed"}
+                  className={styles.tumbleweed}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+          <Box height={"1rem"} />
+          <ArrowLink
+            link={"/"}
+            arrow={"right"}
+            description={"Let's go home?"}
+          />
+        </VStack>
+      </Center>
+
       <Spacer />
     </>
   );
