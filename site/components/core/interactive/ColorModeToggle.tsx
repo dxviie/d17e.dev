4f7741a58e4db9db01c9c88b625c6a65627c5ed1
david <david@d17e.dev>
2023-08-04 12:03:44 +0200

    removed unused colors

--- site/components/core/interactive/ColorModeToggle.tsx
+++ site/components/core/interactive/ColorModeToggle.tsx
@@ -1,9 +1,7 @@
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { Flex, useColorMode } from "@chakra-ui/react";
-import useThemeColors from "../hooks/useThemeColors";
 
 export default function ColorModeToggle() {
-  const colors = useThemeColors();
   const colorModeContext = useColorMode();
   return (
     <>
