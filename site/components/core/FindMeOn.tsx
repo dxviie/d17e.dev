77c1a3f2ee24368be012fded9cb01260ac27f723
david <david@d17e.dev>
2024-03-09 10:02:05 +0100

    social links fixed on phone

--- site/components/core/FindMeOn.tsx
+++ site/components/core/FindMeOn.tsx
@@ -4,27 +4,45 @@ import {GitHubIcon} from "../icons/GitHubIcon";
 import InstaIcon from "../icons/InstaIcon";
 import {TwitterIcon} from "../icons/TwitterIcon";
 import PlainTextLink from "./interactive/PlainTextLink";
+import useIsPhone from "./hooks/useIsPhone";
 
-export default function FindMeOn({direction = "row", title = "Find me on"}: { direction?: "column" | "row", title?: string }) {
-  const listDirection = direction;
+export default function FindMeOn({title = "Find me on"}: { title?: string }) {
+  const listDirection = "row";
   const itemDirection = "row";
-  const gap = direction === "column" ? ".5rem" : "1rem";
-  const gapChar = direction === "column" ? "" : ".";
+  const gap = "1rem";
+  const gapChar = ".";
+  let isPhone = useIsPhone();
   return (
     <>
       <Flex direction={"column"} alignItems={"center"}>
         <Box marginBottom={".5rem"}>
           {title}
         </Box>
-        <Flex direction={listDirection} justifyContent={"center"} alignItems={"flex-start"} gap={gap}>
-          {buildItem(itemDirection, <GitHubIcon/>, "dxviie", "https://github.com/dxviie")}
-          {gapChar}
-          {buildItem(itemDirection, <InstaIcon/>, "d17e.dev", "https://www.instagram.com/d17e.dev/")}
-          {gapChar}
-          {buildItem(itemDirection, <TwitterIcon/>, "david_d17e", "https://twitter.com/david_d17e")}
-          {gapChar}
-          {buildItem(itemDirection, <LinkedInIcon/>, "d16de", "https://www.linkedin.com/in/d16de/")}
-        </Flex>
+        {isPhone ?
+          <>
+            <Flex direction={"row"} gap={"2rem"}>
+              <Flex direction={"column"} justifyContent={"center"} alignItems={"flex-start"} gap={gap}>
+                {buildItem(itemDirection, <GitHubIcon/>, "dxviie", "https://github.com/dxviie")}
+                {buildItem(itemDirection, <TwitterIcon/>, "david_d17e", "https://twitter.com/david_d17e")}
+              </Flex>
+              <Flex direction={"column"} justifyContent={"center"} alignItems={"flex-start"} gap={gap}>
+                {buildItem(itemDirection, <InstaIcon/>, "d17e.dev", "https://www.instagram.com/d17e.dev/")}
+                {buildItem(itemDirection, <LinkedInIcon/>, "d16de", "https://www.linkedin.com/in/d16de/")}
+              </Flex>
+            </Flex>
+          </>
+          :
+          <>
+            <Flex direction={listDirection} justifyContent={"center"} alignItems={"flex-start"} gap={gap}>
+              {buildItem(itemDirection, <GitHubIcon/>, "dxviie", "https://github.com/dxviie")}
+              {gapChar}
+              {buildItem(itemDirection, <InstaIcon/>, "d17e.dev", "https://www.instagram.com/d17e.dev/")}
+              {gapChar}
+              {buildItem(itemDirection, <TwitterIcon/>, "david_d17e", "https://twitter.com/david_d17e")}
+              {gapChar}
+              {buildItem(itemDirection, <LinkedInIcon/>, "d16de", "https://www.linkedin.com/in/d16de/")}
+            </Flex>
+          </>}
       </Flex>
     </>
   );
