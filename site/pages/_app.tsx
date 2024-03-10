452ed8b3a5f7c370c1940f34dbfd8b29a9422df5
david <david@d17e.dev>
2024-03-10 11:15:31 +0100

    umami disabled for dev env

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import "../styles/markdown.css";
 import type {AppProps} from "next/app";
 import {ChakraProvider, Flex, Spacer} from "@chakra-ui/react";
-import Head from "next/head";
 import Footer from "../components/core/layout/Footer";
 import d17eTheme from "../styles/d17eTheme";
 import Navigation from "../components/core/interactive/Navigation";
@@ -10,10 +9,12 @@ import React from "react";
 import {isAtHomePage} from "../services/RouterUtils";
 import {useRouter} from "next/router";
 import Script from "next/script";
+import Head from "next/head";
 
 function MyApp({Component, pageProps}: AppProps) {
   const router = useRouter();
   const scrollSnapping = isAtHomePage(router.asPath) ? "start" : "none";
+  const dev = process.env.NODE_ENV === "development";
   setTimeout(() => {
     // console.clear();
     console.log(
@@ -33,25 +34,27 @@ function MyApp({Component, pageProps}: AppProps) {
   }, 1000);
   return (
     <>
+      {/* headers */}
+      <Head>
+        <title>d17e.dev - code. art. ideas.</title>
+        <meta name="description" content="d17e.dev - code. art. ideas."/>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=12"
+        />
+        <link rel="icon" href="/d17e-favicon.ico"/>
+      </Head>
       {/* umami anonymous traffic tracker */}
-      <Script
-        async={true}
-        strategy="afterInteractive"
-        data-website-id="fefcc6cd-205e-4632-940b-cdea83a38ac8"
-        src={"https://umami.d17e.dev/script.js"}
-      />
+      {dev ? <></> :
+        <Script
+          async={true}
+          strategy="afterInteractive"
+          data-website-id="fefcc6cd-205e-4632-940b-cdea83a38ac8"
+          src={"https://umami.d17e.dev/script.js"}
+        />}
       <React.StrictMode>
         <ChakraProvider theme={d17eTheme} resetCSS={true}>
-          {/* headers */}
-          <Head>
-            <title>d17e.dev - code. art. ideas.</title>
-            <meta name="description" content="d17e.dev - code. art. ideas."/>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-            <link rel="icon" href="/d17e-favicon.ico"/>
-          </Head>
+
           <Flex
             width={"100%"}
             height={"3.5rem"}
