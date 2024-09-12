ffd7abd49c19572ebab3bbf265920cfbde916eb1
david <david@d17e.dev>
2024-09-12 16:55:42 +0200

    migrated page content

--- site/components/content/blog/ArticleFooter.tsx
+++ site/components/content/blog/ArticleFooter.tsx
@@ -1,25 +1,15 @@
-import { HStack } from "@chakra-ui/react";
-import { ArticleDTO } from "../../../services/ContentTypes";
-import useThemeColors from "../../core/hooks/useThemeColors";
+import {HStack} from "@chakra-ui/react";
+import {ArticleDTO} from "../../../services/ContentTypes";
 import Avatar from "../../core/elements/Avatar";
 
-export default function ArticleFooter({ article }: { article: ArticleDTO }) {
-  const colors = useThemeColors();
+export default function ArticleFooter({article}: { article: ArticleDTO }) {
   if (!article) {
     // don't try to render missing article
     return <></>;
   }
   return (
-    <>
-      <HStack justifyContent={"flex-end"}>
-        {article.author.name && article.author.avatar ? (
-          <>
-            <Avatar author={article.author} />
-          </>
-        ) : (
-          <></>
-        )}
-      </HStack>
-    </>
+    <HStack justifyContent={"flex-end"}>
+      <Avatar/>
+    </HStack>
   );
 }
