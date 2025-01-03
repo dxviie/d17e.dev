f26c9e6d52d15ffa35af0ebf1bfcff0e311adf9f
david <david@d17e.dev>
2025-01-03 17:20:17 +0100

    directus content collection for posts

--- site-v2/astro.config.mjs
+++ site-v2/astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+// @ts-ignore
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap()],
 });
--- site-v2/src/content.config.ts
+++ site-v2/src/content.config.ts
@@ -1,5 +1,7 @@
 import {glob} from 'astro/loaders';
 import {defineCollection, z} from 'astro:content';
+import {authentication, createDirectus, readItems, rest} from "@directus/sdk";
+import {getSecret} from "astro:env/server";
 
 const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
@@ -15,4 +17,51 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = {blog};
+const directus = createDirectus(getSecret('DIRECTUS_URL') || '').with(authentication('json')).with(rest());
+const posts = defineCollection({
+  // @ts-ignore
+  loader: async () => {
+    try {
+      const login = await directus.login(getSecret('DIRECTUS_LOGIN') || '', getSecret('DIRECTUS_PASS') || '');
+      console.debug('Logged in', login);
+      let posts = await directus.request(readItems('Posts', {
+        fields: ['*', 'cover.*'],
+        filter: {
+          status: {
+            _eq: 'published'
+          }
+        },
+        limit: -1
+      })) || [{id: '1'}];
+      posts = posts.map(p => ({...p, id: p.uuid}));
+      console.debug('Loaded Posts', posts.length, 'First Post: ', posts[0]);
+      return posts;
+    } catch (error) {
+      console.error('Directus error:', error);
+      return [{id: '1'}];
+    }
+  },
+  schema: z.object({
+    id: z.string(),
+    status: z.string(),
+    dateCreated: z.coerce.date().optional(),
+    dateUpdated: z.coerce.date().optional(),
+    publishedDate: z.coerce.date().optional(),
+    title: z.string(),
+    body: z.string(),
+    link: z.string().nullable().optional(),
+    linkDescription: z.string().nullable().optional(),
+    slug: z.string(),
+    cover: z.object({
+      id: z.string(),
+      title: z.string(),
+      description: z.string().nullable().optional(),
+      filenameDownload: z.string().nullable().optional(),
+      type: z.string(),
+      width: z.number().nullable().optional(),
+      height: z.number().nullable().optional(),
+    }),
+  }),
+});
+
+export const collections = {blog, posts};
