3e0a5cba9665ee2c3a1faf210d719e2479fc75a0
david <david@d17e.dev>
2025-03-05 22:49:15 +0100

    dice sizing

--- site-v2/src/components/bento/LandingBentoGrid.svelte
+++ site-v2/src/components/bento/LandingBentoGrid.svelte
@@ -212,6 +212,20 @@
     };
     if (isWide) {
       bentoContent.push(aboutBox);
+    } else {
+      bentoContent.push({
+        id: 'about-link',
+        dimensions: [{width: 1, height: 1}],
+        html: `
+      <a href="https://forms.d17e.dev/contact" target="_self" class="link-link"><div class="link-container">
+        <div class="svg-container">
+          <svg width="90%" height="90%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="rotating-contact">
+            <path d="M3.29289 5.29289C3.47386 5.11193 3.72386 5 4 5H20C20.2761 5 20.5261 5.11193 20.7071 5.29289M3.29289 5.29289C3.11193 5.47386 3 5.72386 3 6V18C3 18.5523 3.44772 19 4 19H20C20.5523 19 21 18.5523 21 18V6C21 5.72386 20.8881 5.47386 20.7071 5.29289M3.29289 5.29289L10.5858 12.5857C11.3668 13.3668 12.6332 13.3668 13.4142 12.5857L20.7071 5.29289" stroke="${bentoConfig.bgColor}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </div>
+      </div></a>`,
+        required: true
+      });
     }
 
     bentoContent.push({
@@ -553,6 +567,16 @@
     :global(.dice-svg) {
         animation: dice-rotate 30s linear infinite;
 
+    }
+
+    :global(.icon-font) {
+        font-family: 'bariol_icons', serif;
+        margin-top: 3rem;
+        margin-left: 1rem;
+    }
+
+    :global(.rotating-contact) {
+        animation: rotate 24s ease-in-out infinite;
     }
 
     @keyframes etator {
