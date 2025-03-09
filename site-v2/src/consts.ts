42251a0370182d5c40c169704c9dbd7510850e24
david <david@d17e.dev>
2025-03-09 12:31:00 +0100

    og headers improvements

--- site-v2/src/consts.ts
+++ site-v2/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'd17e.dev';
-export const SITE_DESCRIPTION = 'code. art. ideas.';
+export const SITE_TITLE = 'd17e.dev - code. art. ideas.';
+export const SITE_DESCRIPTION = 'd17e is David Vandenbogaerde. This is his corner of the internet.';
