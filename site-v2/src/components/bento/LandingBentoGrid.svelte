bf8221bc43198a0e6f9bd73a5959c0ca171a692f
david <david@d17e.dev>
2025-03-21 14:40:54 +0100

    projects dark launch

--- site-v2/src/components/bento/LandingBentoGrid.svelte
+++ site-v2/src/components/bento/LandingBentoGrid.svelte
@@ -531,30 +531,30 @@
       required: true
     })
 
-    bentoContent.push({
-      id: 'projects-link',
-      dimensions: [{width: 1, height: 1}],
-      html: `
-      <a href="/projects" target="_self" class="link-link" data-umami-event="lp-click-projects" aria-label="Projects..."><div class="link-container">
-        <div class="svg-container">
-          <svg
-                  class="projects-svg rotating-svg"
-                  width="85%"
-                  height="85%"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  viewBox="0 0 24 24"
-          >
-            <path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"/>
-            <path d="M12 15l-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z"/>
-            <path d="M9 12H4s.55-3.03 2-4c1.45-.97 2-.5 2-.5"/>
-            <path d="M12 15v5s3.03-.55 4-2c.97-1.45.5-2 .5-2"/>
-          </svg>
-        </div></div>
-      </a>`,
-      required: true
-    });
+    // bentoContent.push({
+    //   id: 'projects-link',
+    //   dimensions: [{width: 1, height: 1}],
+    //   html: `
+    //   <a href="/projects" target="_self" class="link-link" data-umami-event="lp-click-projects" aria-label="Projects..."><div class="link-container">
+    //     <div class="svg-container">
+    //       <svg
+    //               class="projects-svg rotating-svg"
+    //               width="85%"
+    //               height="85%"
+    //               strokeWidth="2"
+    //               strokeLinecap="round"
+    //               strokeLinejoin="round"
+    //               viewBox="0 0 24 24"
+    //       >
+    //         <path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"/>
+    //         <path d="M12 15l-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z"/>
+    //         <path d="M9 12H4s.55-3.03 2-4c1.45-.97 2-.5 2-.5"/>
+    //         <path d="M12 15v5s3.03-.55 4-2c.97-1.45.5-2 .5-2"/>
+    //       </svg>
+    //     </div></div>
+    //   </a>`,
+    //   required: true
+    // });
 
     bentoContent.push({
       id: 'dice',
